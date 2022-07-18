8ab66f29bdbebe5806f6e8cc1d7b7a287f755f7a
Chiraag Balu <chiraagbalu@berkeley.edu>
2022-07-18 14:52:52 -0700

    added alertwh.py - uses webhooks

--- alertwh.py
+++ alertwh.py
@@ -4,60 +4,82 @@ import os
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
+import psycopg2
 from dotenv import load_dotenv
+import asyncio
+import traceback
 load_dotenv()
 
+# channel url
 url = "https://discord.com/api/webhooks/998686560495095848/kHPDCW_ecMhqlraKcBeCwdxhrUQ9u5m8NEpgAVrLfyIoyhov8A5dX_JNcyCsjNj5MRd2"
+# set up webhook using the url
 webhook = Webhook.from_url(url, adapter=RequestsWebhookAdapter())
+# send test message to url
 webhook.send("Hello World")
 
+# connection credentials
 db_name = os.getenv('DB_NAME')
 db_user = os.getenv('DB_USER')
 db_pass = os.getenv('DB_PASS')
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 
-stale_time = 86400
-max_leverage = 200
-channelid = 997266071167971372
-table = 'mm_data'
+# connection
+try:
+    conn = psycopg2.connect(
+        host=db_host,
+        database=db_name,
+        user=db_user,
+        password=db_pass,
+    )
+    cur = conn.cursor()
+except Exception as e:
+    print("ERROR: Unable to connect to database:", traceback.print_exc())
 
-dbstring = (
-    f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}")
+# check data
 
-sqlconn = create_engine(dbstring).connect()
 
-mmData = pd.read_sql_table(table, sqlconn)
-df = mmData
+async def checkStaleData(timeout=60, staletime=86400, market='BTC-PERP'):
+    while True:
+        # create sql query
+        def query():
+            return f"""
+            SELECT "time", "marketName" FROM public.mm_data
+            WHERE "marketName" = '{market}'
+            ORDER BY id DESC LIMIT 1
+            """
+        # run sql query
+        cur.execute(query())
 
-markets = df.marketName.unique()
-for market in markets:
-    lastData = np.array(df[df.marketName == market]
-                        ['time'].sort_values(ascending=False))[0]
-    lastPrice = np.array(
-        df[df.marketName == market]['oraclePrice'].sort_values(ascending=False))[0]
-    lastDataTime = lastData.timestamp()
-    now = pd.to_datetime('now').timestamp()
-    timesincelastdata = now-lastDataTime
-    print(f'info for: {market}')
-    print(f'last data: {lastData}')
-    print(f'now: {pd.to_datetime("now")}')
-    print(f'hours since last data: {(timesincelastdata)/3600}')
-    if timesincelastdata > stale_time:
-        webhook.send(
-            f'stale data warning for {market}: hours since last data: {(timesincelastdata)/3600}')
-    else:
-        webhook.send(f'{market} data up to date!')
-    lastBasePos = np.array(
-        df[df.marketName == market]['basePos'].sort_values(ascending=False))[0]
-    lastNotionalDelta = lastBasePos * lastPrice
-    lastLeverage = np.array(
-        df[df.marketName == market]['leverage'].sort_values(ascending=False))[0]
-    print(f'last notional delta: {lastNotionalDelta}')
-    print(f'last leverage: {lastLeverage}%')
-    if abs(lastLeverage) > max_leverage:
-        webhook.send(
-            f'leverage warning for {market}: current leverage: {lastLeverage}%')
-    else:
-        webhook.send(f'{market} leverage is within bounds!')
-    print('='*35)
+        # grab timestamp
+        rows = cur.fetchall()
+        time = rows[0][0]
+        lastTimeStamp = time.timestamp()
+
+        # find current timestamp
+        now = pd.to_datetime('now').timestamp()
+
+        # compare
+        timesincelastdata = now-lastTimeStamp
+
+        # send discord message using webhook based on result
+        if timesincelastdata > staletime:
+            webhook.send(
+                f'{market}: hours since last data: {(timesincelastdata)/3600}')
+        else:
+            webhook.send(f'{market}: up to date!')
+
+        await asyncio.sleep(timeout)
+
+# call function for each market
+
+
+async def main():
+    await asyncio.gather(checkStaleData(timeout=60, staletime=86400, market='BTC-PERP'), checkStaleData(timeout=60, staletime=86400, market='BTC^2-PERP'), checkStaleData(timeout=60, staletime=86400, market='BTC_1D_IV-PERP'))
+
+# helps export function to be run
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+conn.close()
